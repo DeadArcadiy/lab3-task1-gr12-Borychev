e224c095951c72d47b89e1796de109b499e93e9e
Denis Borychev <108476071+DeadArcadiy@users.noreply.github.com>
2023-03-05 15:16:06 +0000

    Task1 variant 3 done

--- src/main.c
+++ src/main.c
@@ -1,8 +1,34 @@
-/* hello.c */
 #include <stdio.h>
-
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+ 
+ 
+ 
+bool simple (int n) {
+ for(int i = 2; i <= n/2; i++)
+   {
+     if(n%i == 0)
+     {
+       return 0;
+     }
+   }
+  return 1;
+}
+ 
+int main () {
+ int a,b;
+  b = 3;
+ scanf("%d", &a);
+ for (int i=0; i<=a; i++){
+    if(simple(i))
+    {
+      if(b + 2 == i)
+      {
+        printf("%d %d\n",b,i);
+      }
+      b = i;
+    }
+ }
+ return 0;
 }
